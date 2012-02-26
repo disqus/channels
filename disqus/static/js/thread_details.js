7c67abbf4ee393debe39571f3ad4de5830fe1dfa
Matthew Hooker <mwhooker@gmail.com>
2012-02-25 20:29:54 -0800

    scroll to bottom on page load

--- disqus/static/js/thread_details.js
+++ disqus/static/js/thread_details.js
@@ -5,6 +5,7 @@
 
     function Details() {
       var _this = this;
+      this.scrollBottom();
       $('#message').keydown(function(e) {
         if (e.which === 13 && !e.shiftKey) {
           _this.submit();
@@ -15,6 +16,12 @@
 
     Details.prototype.submit = function() {
       return $('form')[0].submit();
+    };
+
+    Details.prototype.scrollBottom = function() {
+      return $('body').animate({
+        scrollTop: $(document).height()
+      }, "slow");
     };
 
     return Details;
