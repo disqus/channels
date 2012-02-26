342d297a530d74b8c5c999437c293e1b98274c7f
David Cramer <dcramer@gmail.com>
2012-02-25 18:04:21 -0800

    Serialize dates and add thread info to sessions

--- setup_threads.py
+++ setup_threads.py
@@ -34,34 +34,38 @@ if not talk_category_id:
 
 print "Downloading list of existing threads..."
 paginator = Paginator(disqusapi.threads.list, **api_args(category=talk_category_id))
-existing_urls = [thread['link'] for thread in paginator]
+existing_urls = dict((t['link'], t) for t in paginator)
 
 print "%s existing threads found!" % len(existing_urls)
 
 print 'Downloading and processing sessionlist...'
 schedule = json.loads(urllib.urlopen('https://us.pycon.org/2012/schedule/json/').read())
-f = open('sessions.pickle', 'w')
-pickle.dump(schedule, f, pickle.HIGHEST_PROTOCOL)
-f.close()
+schedule = [s for s in schedule if s.get('url')]
 
 for talk in schedule:
     print ' - Processing session %s' % talk['title']
 
-    if not talk['url'] or talk['url'] in existing_urls:
-        continue
-
     for key in ('start', 'end', 'last_updated'):
-        talk[key] = datetime.datetime(*map(int, talk['start'][0:6]))
+        talk[key] = datetime.datetime(*map(int, talk[key][0:6]))
 
-    args = dict(
-        forum=app.config['DISQUS_FORUM'],
-        title=talk['title'].encode('utf-8'),
-        url=talk['url'],
-        date=talk['start'],
-        category=talk_category_id,
-        message=talk['description'].encode('utf-8')
-    )
+    if talk['url'] not in existing_urls:
+        args = dict(
+            forum=app.config['DISQUS_FORUM'],
+            title=talk['title'].encode('utf-8'),
+            url=talk['url'],
+            date=talk['start'],
+            category=talk_category_id,
+            message=talk['description'].encode('utf-8')
+        )
 
-    api(disqusapi.threads.create, **args)
+        thread = api(disqusapi.threads.create, **args)
+    else:
+        thread = existing_urls[talk['url']]
+    talk['disqus:thread'] = thread
+
+print 'Saving sessions to disk'
+with open('sessions.pickle', 'wb') as f:
+    pickle.dump(schedule, f, pickle.HIGHEST_PROTOCOL)
+
 
 print "ALL DONE!"
