99e27bed98576e746eee7b7502ab1682c47fb3ef
David Cramer <dcramer@gmail.com>
2012-03-04 23:07:04 -0800

    fab deploy

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import env, local, cd, run, sudo
+from os.path import expanduser, join
+
+
+INSTALL_PATH = '/home/ec2-user/minisite/current'
+
+env.hosts = ['pycon.disqus.com']
+env.key_filename = [expanduser('~/.ssh/minisite.pem')]
+env.user = 'ec2-user'
+
+
+def deploy():
+    with cd(INSTALL_PATH):
+        run('git pull origin master')
+
+        run('../venv/bin/pip install -e .')
+
+    #ln -s $INSTALL_DIR/local_settings.py disqus/local_settings.py
+
+    sudo('supervisorctl restart all')
+
+
+ # ssh -i ~/.ssh/minisite.pem ec2-user@23.21.178.71
