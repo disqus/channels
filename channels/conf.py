c72c86a05ecfb1a75122da383c05c9981569d0e9
David Cramer <dcramer@gmail.com>
2012-03-21 14:35:55 -0700

    Remove default secret key so its obvious it needs set

--- channels/conf.py
+++ channels/conf.py
@@ -21,7 +21,7 @@ class DefaultConfig(object):
 
     }
     SITE_NAME = 'PyCon 2012'
-    SECRET_KEY = '\x92\x99\x1d\x9c\x95\xb8~\x8b\xf8k{\x90oX\t\x87\xd9)2i\xf5\x0c\xd4k'
+    SECRET_KEY = None
     DISQUS_PUBLIC = None
     DISQUS_SECRET = None
     DISQUS_FORUM = None
